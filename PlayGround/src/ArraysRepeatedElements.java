23479baf27e84c99988c8a11711a34e2997e93ad
Vignesh <techvicky2018@gmail.com>
2024-04-14 23:48:48 +0530

    Repeated elements found using Array Index

--- PlayGround/src/ArraysRepeatedElements.java
+++ PlayGround/src/ArraysRepeatedElements.java
@@ -1,17 +1,9 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
-public class ArraysPg {
+public class ArraysRepeatedElements {
 
-    // Function that print the repeated element and the missing element in an array of n elements,
-    // provided elements of the array are in range of [1,n]
-    // Map Approach
-    //https://leetcode.com/problems/find-missing-and-repeated-values/description/
     public static void RepeatedMissingUsingMap(int[] arr) {
-
         int n = arr.length;
         HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -40,7 +32,6 @@ public class ArraysPg {
         }
 
         System.out.println("RepeatedMissingUsingMap\nRepeat : " + repeat + " | Missing: " + missing);
-
     }
 
     public static List<Integer> findDuplicates(int[] nums) {
@@ -52,6 +43,53 @@ public class ArraysPg {
                 .collect(Collectors.toList());
     }
 
+    public static void printRepeating(int[] arr, int size) {
+        int xor = 0;
+        int set_bit_no;
+        int n = size - 2;
+        int x = 0, y = 0;
+
+        for (int i = 0; i < size; i++) {
+            xor ^= arr[i];
+        }
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
+        }
+
+        set_bit_no = (xor & ~(xor - 1));
+
+        for (int i = 0; i < size; i++) {
+            if ((arr[i] & set_bit_no) != 0) {
+                x ^= arr[i];
+            } else {
+                y ^= arr[i];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if ((i & set_bit_no) != 0) {
+                x ^= i;
+            } else {
+                y ^= i;
+            }
+        }
+
+        System.out.print("Repeating elements are ");
+        System.out.println(x + " " + y + " using XOR");
+    }
+
+    public static void repeatedElementsUsingArrayIndex(int[] nums, int n) {
+        System.out.print("Repeated elements found using Array Index are :");
+        for (int i = 0; i < n; i++) {
+            int abs_val = Math.abs(nums[i]);
+            if (nums[abs_val] > 0) {
+                nums[abs_val] = -nums[abs_val];
+            } else {
+                System.out.print(" " + i);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] arr = {2, 1, 3, 4, 4};
         RepeatedMissingUsingMap(arr);
@@ -62,6 +100,11 @@ public class ArraysPg {
         System.out.println("Two Duplicates using stream : " + two_duplicates);
         System.out.println("---------------------------\n");
 
+        int[] arr2 = {4, 2, 4, 5, 2, 3, 1};
+        int arr_size = arr2.length;
+        printRepeating(arr2, arr_size);
+        repeatedElementsUsingArrayIndex(arr2, arr_size);
+
+
     }
 }
-
