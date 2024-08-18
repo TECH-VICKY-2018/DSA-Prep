b4b1d9dcc6f4cbc497a610a350f4bcbc2b733208
Vignesh <techvicky2018@gmail.com>
2024-08-18 17:49:10 +0530

    8/18/2024

--- PlayGround/src/arrays/ZerosToEnd.java
+++ PlayGround/src/arrays/ZerosToEnd.java
@@ -3,22 +3,20 @@ package arrays;
 public class ZerosToEnd {
 
     public void moveZeroes(int[] nums) {
-        int n = nums.length, i = 0, j = 1;
+        int n = nums.length;
+        int j = 0; // Pointer to track the position of the next non-zero position in the array
 
-        while (i < n && j < n) {
-
-            if (nums[i] == 0 && nums[j] == 0) {
-                j++;
+        // Iterate through the array with the first pointer
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                // Swap only when i and j are different
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                j++; // Move the j pointer to the next position
             }
-
-            if (nums[i] == 0 && nums[j] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
-                j++;
-            }
-
         }
 
     }
